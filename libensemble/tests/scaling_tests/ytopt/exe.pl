b0805d95d82d349e11b8fd21d23a57b500994768
jlnav <jnavarro@anl.gov>
2022-02-04 11:43:32 -0600

    simple attempt for workers to use fractions of multiple nodes

--- libensemble/tests/scaling_tests/ytopt/exe.pl
+++ libensemble/tests/scaling_tests/ytopt/exe.pl
@@ -4,7 +4,7 @@
 #MCS, ANL
 # exe.pl: average the execution time in 5 runs
 #
-use Time::HiRes qw(gettimeofday); 
+use Time::HiRes qw(gettimeofday);
 
 foreach $filename (@ARGV) {
  #  print "Start to preprocess ", $filename, "...\n";
@@ -12,8 +12,10 @@ foreach $filename (@ARGV) {
    $nmax = 1;
    @nn = (1..$nmax);
    for(@nn) {
-    $retval = gettimeofday( ); 
-    system("aprun -n 8 -N 1 $filename >/dev/null 2>&1");
+    $retval = gettimeofday( );
+    # 16 MPI ranks total, 8 ranks per node, 1 thread per rank, 1 thread
+    # aprun splits this into 2 nodes (16 ranks / 8 ranks-per-node)
+    system("aprun -n 16 -N 8 -d 1 -j 1 $filename >/dev/null 2>&1");
     $tt = gettimeofday( );
     $ttotal = $tt - $retval;
     $ssum = $ssum + $ttotal;
