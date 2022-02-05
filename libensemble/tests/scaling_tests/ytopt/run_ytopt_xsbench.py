d48a5ef633ee05e09b40e48572bd34510f7dbf4c
jlnav <jnavarro@anl.gov>
2022-02-05 09:55:43 -0600

    fix syntax error

--- libensemble/tests/scaling_tests/ytopt/run_ytopt_xsbench.py
+++ libensemble/tests/scaling_tests/ytopt/run_ytopt_xsbench.py
@@ -121,4 +121,4 @@ if is_manager:
         dtypes = H[gen_specs['persis_in']].dtype
         b = np.vstack(map(list, H[gen_specs['persis_in']]))
         print(b)
-        np.savetxt('Output.csv',b, header=','.join(dtypes.names), delimiter=',',fmt=','.join(['%s']*b.shape[1]))s)
+        np.savetxt('Output.csv',b, header=','.join(dtypes.names), delimiter=',',fmt=','.join(['%s']*b.shape[1]))
